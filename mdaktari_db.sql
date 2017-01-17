7fc4361ba82b38dce85c65d9d4e1d84f3e34b697
Griffin Muteti <griffinmuteti31@gmail.com>
2017-01-17 12:10:32 +0300

    add mdaktari database

---
+++ mdaktari_db.sql
@@ -0,0 +1,182 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jan 17, 2017 at 09:51 WD
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mdaktari_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointment_calendar`
+--
+
+CREATE TABLE `appointment_calendar` (
+  `appointment_id` int(11) NOT NULL,
+  `appointment_title` varchar(200) NOT NULL,
+  `appointment_desc` varchar(500) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `patient_id` int(11) NOT NULL,
+  `doctor_id` int(11) NOT NULL,
+  `appointment_status` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor_calendar`
+--
+
+CREATE TABLE `doctor_calendar` (
+  `doctor_id` int(11) NOT NULL,
+  `time` date NOT NULL,
+  `date` time NOT NULL,
+  `availability` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor_reviews`
+--
+
+CREATE TABLE `doctor_reviews` (
+  `review_id` int(11) NOT NULL,
+  `doctor_id` int(11) NOT NULL,
+  `review_title` varchar(200) NOT NULL,
+  `review_desc` varchar(500) NOT NULL,
+  `review_number` int(11) NOT NULL,
+  `patient_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor_table`
+--
+
+CREATE TABLE `doctor_table` (
+  `doctor_id` int(11) NOT NULL,
+  `fullname` varchar(200) NOT NULL,
+  `qualification` varchar(200) NOT NULL,
+  `speciality` varchar(200) NOT NULL,
+  `resume_url` varchar(200) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `town` int(11) NOT NULL,
+  `country` int(11) NOT NULL,
+  `postal_address` varchar(200) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `id_number` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient_table`
+--
+
+CREATE TABLE `patient_table` (
+  `patient_id` int(11) NOT NULL,
+  `fullname` varchar(200) NOT NULL,
+  `dob` date NOT NULL,
+  `sex` varchar(200) NOT NULL,
+  `town` varchar(200) NOT NULL,
+  `country` varchar(200) NOT NULL,
+  `postal_address` varchar(200) NOT NULL,
+  `occupation` varchar(200) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `user_type` varchar(200) NOT NULL,
+  `signup_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `login_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `appointment_calendar`
+--
+ALTER TABLE `appointment_calendar`
+  ADD PRIMARY KEY (`appointment_id`);
+
+--
+-- Indexes for table `doctor_reviews`
+--
+ALTER TABLE `doctor_reviews`
+  ADD PRIMARY KEY (`review_id`);
+
+--
+-- Indexes for table `doctor_table`
+--
+ALTER TABLE `doctor_table`
+  ADD PRIMARY KEY (`doctor_id`);
+
+--
+-- Indexes for table `patient_table`
+--
+ALTER TABLE `patient_table`
+  ADD PRIMARY KEY (`patient_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `appointment_calendar`
+--
+ALTER TABLE `appointment_calendar`
+  MODIFY `appointment_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `doctor_reviews`
+--
+ALTER TABLE `doctor_reviews`
+  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `doctor_table`
+--
+ALTER TABLE `doctor_table`
+  MODIFY `doctor_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `patient_table`
+--
+ALTER TABLE `patient_table`
+  MODIFY `patient_id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
